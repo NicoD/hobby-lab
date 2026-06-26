4a2d28432f634e4d7f9035b7c6762b4e24b3b13b
NicoD <n.denoual@gmail.com>
2026-06-26 10:45:31 +0200

    ci:  pretty for js app (user and frontend)

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -36,4 +37,5 @@ export default defineConfig([
       },
     },
   },
+  prettierConfig,
 ])
